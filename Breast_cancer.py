48ad3bfb6ffd8efd9c9b1bffbe7edbaceb12a6b3
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-15 05:14:37 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -1,43 +1,79 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 tf.random.set_seed(3)
 from tensorflow import keras
+import streamlit as st
 
-# Load the saved model
-model = keras.models.load_model('model.h5')
+# loading the data from sklearn
+breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
 
-# Set the page title
-st.title("Breast Cancer Prediction using Machine Learning")
+# loading the data to a data frame
+data_frame = pd.DataFrame(breast_cancer_dataset.data, columns = breast_cancer_dataset.feature_names)
 
-# Create a file uploader for CSV input
-csv_file = st.file_uploader("Upload CSV file", type="csv")
+# adding the 'target' column to the data frame
+data_frame['label'] = breast_cancer_dataset.target
 
-if csv_file is not None:
-    # Read the uploaded CSV file
-    input_df = pd.read_csv(csv_file)
+X = data_frame.drop(columns='label', axis=1)
+Y = data_frame['label']
 
-    # Create input fields for features
-    feature_columns = input_df.columns.tolist()
-    input_data = {}
+#splitting the data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
 
-    for column in feature_columns:
-        input_value = st.text_input(column)
-        input_data[column] = input_value
+#Standardize the data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_std = scaler.fit_transform(X_train)
+X_test_std = scaler.transform(X_test)
 
-    # Create a button for prediction
-    if st.button("Predict"):
-        # Convert input values to appropriate data types
-        input_values = [float(input_data[column]) if input_data[column] != '' else np.nan for column in feature_columns]
-        input_array = np.array([input_values], dtype=np.float32)
+# importing tensorflow and Keras
+import tensorflow as tf
+tf.random.set_seed(3)
+from tensorflow import keras
+# setting up the layers of Neural Network
 
-        # Perform the prediction
-        prediction = model.predict(input_array)
-        predicted_class = np.argmax(prediction)
+model = keras.Sequential([
+                          keras.layers.Flatten(input_shape=(30,)),
+                          keras.layers.Dense(20, activation='relu'),
+                          keras.layers.Dense(2, activation='sigmoid')
+])
+# training the neural Network
 
-        # Map the predicted class to diagnosis
-        diagnosis = 'Malignant' if predicted_class == 0 else 'Benign'
+history = model.fit(X_train_std, Y_train, validation_split=0.1, epochs=10)
+loss, accuracy = model.evaluate(X_test_std, Y_test)
 
-        # Display the diagnosis
-        st.success(f"The tumor is {diagnosis}")
+Y_pred = model.predict(X_test_std)
+
+input_data = ([13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259
+])
+# change the input_data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for one data point
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardizing the input data
+input_data_std = scaler.transform(input_data_reshaped)
+
+prediction = model.predict(input_data_std)
+
+prediction_label = [np.argmax(prediction)]
+
+if(prediction_label[0] == 0):
+  st.write('The tumor is Malignant')
+
+else:
+  st.write('The tumor is Benign')
+
+
+
+
+
+
+
+
+
+
