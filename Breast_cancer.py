be01c804662304f7ebe9006c3e10a8e30f33b597
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-14 21:03:13 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -53,7 +53,7 @@ features = [[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_me
 # Perform prediction
 if st.button("Predict"):
     prediction = model.predict(features)
-    if prediction == 0:
+    if (predictions == 0).any():
         st.success("The tumor is Malignant")
     else:
         st.success("The tumor is Benign")
