995c1d05b9fc8317ae59797bc923359b8e43df09
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-14 23:52:16 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -11,29 +11,30 @@ st.title("Breast Cancer Prediction using TensorFlow and Keras")
 
 # Create input fields for features
 feature_names = [
-    "Radius Mean", "Texture Mean", "Perimeter Mean", "Area Mean", "Smoothness Mean",
-    "Compactness Mean", "Concavity Mean", "Concave Points Mean", "Symmetry Mean",
-    "Fractal Dimension Mean", "Radius SE", "Texture SE", "Perimeter SE", "Area SE",
-    "Smoothness SE", "Compactness SE", "Concavity SE", "Concave Points SE",
-    "Symmetry SE", "Fractal Dimension SE", "Radius Worst", "Texture Worst",
-    "Perimeter Worst", "Area Worst", "Smoothness Worst", "Compactness Worst",
-    "Concavity Worst", "Concave Points Worst", "Symmetry Worst", "Fractal Dimension Worst"
+    "radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean",
+    "compactness_mean", "concavity_mean", "concave_points_mean", "symmetry_mean",
+    "fractal_dimension_mean", "radius_se", "texture_se", "perimeter_se", "area_se",
+    "smoothness_se", "compactness_se", "concavity_se", "concave_points_se",
+    "symmetry_se", "fractal_dimension_se", "radius_worst", "texture_worst",
+    "perimeter_worst", "area_worst", "smoothness_worst", "compactness_worst",
+    "concavity_worst", "concave_points_worst", "symmetry_worst",
+    "fractal_dimension_worst"
 ]
+input_data = []
 
-feature_values = []
-for feature_name in feature_names:
-    feature_value = st.text_input(feature_name)
-    feature_values.append(feature_value)
+for feature in feature_names:
+    value = st.text_input(feature)
+    input_data.append(float(value))
 
 # Create a button for prediction
 if st.button("Predict"):
-    # Convert input values to appropriate data type
-    input_data = np.array([feature_values], dtype=float)
+    # Create an input array for prediction
+    input_data = np.array([input_data])
 
     # Perform the prediction
     prediction = breast_cancer_model.predict(input_data)
     diagnosis = "Malignant" if prediction[0][0] > 0.5 else "Benign"
-
+    
     # Display the diagnosis
     st.success(f"The tumor is {diagnosis}")
 
