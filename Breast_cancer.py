53ca91246b7b24f772a7c835811f3a8625508bcb
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-15 04:46:56 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import pickle
 
 # Load the trained model
 from tensorflow.keras.models import load_model
@@ -9,32 +9,36 @@ from tensorflow.keras.models import load_model
 # Load the model
 model = load_model("model.h5")
 
-
 # Set the page title
 st.title("Breast Cancer Prediction using Machine Learning")
 
-input_values = st.text_input('ENTER FEATURES SEPERATED BY COMA:')
+# Create a file uploader for CSV input
+csv_file = st.file_uploader("Upload CSV file", type="csv")
 
-input_list = input_values.split(',')
+if csv_file is not None:
+    # Read the uploaded CSV file
+    input_df = pd.read_csv(csv_file)
 
-  
-input_data = np.array([input_list], dtype=np.float32)
+    # Create input fields for features
+    feature_columns = input_df.columns.tolist()
+    input_data = {}
 
+    for column in feature_columns:
+        input_value = st.text_input(column)
+        input_data[column] = input_value
 
-st.write(input_data)
+    # Create a button for prediction
+    if st.button("Predict"):
+        # Convert input values to appropriate data types
+        input_values = [float(input_data[column]) if input_data[column] != '' else np.nan for column in feature_columns]
+        input_array = np.array([input_values], dtype=np.float32)
 
-     # Create a button for prediction
-if st.button("Predict"):
+        # Perform the prediction
+        prediction = model.predict(input_array)
+        predicted_class = np.argmax(prediction)
 
-    prediction = model.predict(input_data)
-    st.write(prediction)
+        # Map the predicted class to diagnosis
+        diagnosis = 'Malignant' if predicted_class == 0 else 'Benign'
 
-    prediction_label = [np.argmax(prediction)]
-    st.write(prediction_label)
-
-    if(prediction_label[0] == 0):
-            st.success('The tumor is Malignant')
-
-    else:
-            st.success('The tumor is Benign')
-
+        # Display the diagnosis
+        st.success(f"The tumor is {diagnosis}")
