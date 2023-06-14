a0e4e55293451bd216da93752d274f18b88df019
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-15 02:44:45 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -36,10 +36,11 @@ if csv_file is not None:
         input_data_reshaped = input_array.reshape(1,-1)
         
         # standardizing the input data
-        input_data_std = scaler.fit_transform(input_data_reshaped)
+        input_data_std = scaler.fit(input_data_reshaped)
+        input_data_std = scaler.transform(input_data_reshaped)
         
         # Perform the prediction
-        prediction = model.predict(input_array)
+        prediction = model.predict(input_data_std)
         predicted_class = np.argmax(prediction)
 
         # Map the predicted class to diagnosis
