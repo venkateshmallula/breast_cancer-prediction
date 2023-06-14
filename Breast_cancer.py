e43b173fc10c60d04bb066d88653e73d7140fc42
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-15 05:21:43 +0530

    Update Breast_cancer.py

--- Breast_cancer.py
+++ Breast_cancer.py
@@ -51,8 +51,13 @@ loss, accuracy = model.evaluate(X_test_std, Y_test)
 
 Y_pred = model.predict(X_test_std)
 
-input_data = ([13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259
-])
+input_value = st.text_input("Enter the features separated by ','")
+
+input_list = input_value.split(',')
+
+input_data = np.array([input_list], dtype=np.float32)
+st.write(input_data)
+
 # change the input_data to a numpy array
 input_data_as_numpy_array = np.asarray(input_data)
 
